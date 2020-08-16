8f7326dad52104e8b511a19a534c6e1bb647bef2
abdelaziz saroui <saroui2015gmail.com>
2020-08-17 00:05:52 +0100

    add search by date in dashboard

--- comtrafic_web/routes.py
+++ comtrafic_web/routes.py
@@ -17,31 +17,36 @@ def dashboard_db_info():
 	res = raw_res.json()
 	c_communications = res['Data']['Lines']
 	data = res['Data']['Data']
-	c = Counter(el['SE_NOM'] for el in data)
-	services = [{'service':key, 'count':value} for key,value in c.items()]
-	services_set = set()
+	services = set()
 	postes = set()
 	pbx = set()
 	for item in data:
-		services_set.add(item['SE_NOM'])
+		services.add(item['SE_NOM'])
 		postes.add(item['CO_EXT'])
 		pbx.add(item['CO_PBX'])
-	c_services = len(services_set)
+	c_services = len(services)
 	c_postes = len(postes)
 	c_pbx = len(pbx)
+	return jsonify({"c_communications":c_communications, "c_postes": c_postes, "c_services": c_services, "c_pbx":c_pbx})
+
+@app.route("/dashboard-data")
+def dashboard_data():
+	if len(request.args) > 0:
+		co_date = request.args.get('CO_DATE') if len(request.args.get('CO_DATE')) else periode
+		raw_res = requests.get(f"{api_url}ED&CO_DATE={co_date}")
+	else:
+		raw_res = requests.get(f"{api_url}ED&CO_DATE={periode}")
+	res = raw_res.json()
+	c_all = res['Data']['Lines']
+	data = res['Data']['Data']
+	c = Counter(el['SE_NOM'] for el in data)
+	services = [{'service':key, 'count':value} for key,value in c.items()]
+	services_set = {service for service,_ in c.items()}
 	couts_raw = {service:0 for service in services_set}
 	for el in data:
 		couts_raw[el['SE_NOM']] += el['CO_COUTFACT_TTC']
 	couts = {k: v for k, v in sorted(couts_raw.items(), key=lambda item: item[1], reverse=True)}
 	top_couts = [{'service':key, 'cout':value} for key,value in couts.items()][:5]
-	return jsonify({"c_communications":c_communications, "c_postes": c_postes, "c_services": c_services, "c_pbx":c_pbx, "services": services, "top_couts": top_couts})
-
-@app.route("/dashboard-data")
-def dashboard_data():
-	raw_res = requests.get(f"{api_url}ED&CO_DATE={periode}")
-	res = raw_res.json()
-	c_all = res['Data']['Lines']
-	data = res['Data']['Data']
 	c_entr = c_sort = 0
 	d_all = d_entr = d_sort = 0;
 	for item in data:
@@ -61,7 +66,9 @@ def dashboard_data():
 	return jsonify({
 			"all": {"c": c_all, "d": d_all_str},
 			"entr": {"c": c_entr, "d": d_entr_str},
-			"sort": {"c": c_sort, "d": d_sort_str}
+			"sort": {"c": c_sort, "d": d_sort_str},
+			"services": services,
+			"top_couts": top_couts
 		})
 		
 	
@@ -79,7 +86,6 @@ def com_data():
 		search_query = ""
 		for el in request.args:
 			search_query += el + "=" + request.args.get(el) + "&"
-		print(search_query)
 		raw_res = requests.get(f"{api_url}ED&CO_DATE={periode}&{search_query}")
 	else:
 		raw_res = requests.get(f"{api_url}ED&CO_DATE={periode}")
