b15764d1f59b623e71d1099eba2337da444bfdae
abdelaziz saroui <saroui2015gmail.com>
2020-08-20 00:42:28 +0100

    Supervision page load postes done

--- comtrafic_web/routes.py
+++ comtrafic_web/routes.py
@@ -133,9 +133,29 @@ def cumuls_data():
 
 @app.route("/supervision")
 def supervision():
-	response = requests.get("https://data.saroui.com/superv.json")
-	data = response.json()
-	return render_template("supervision.html", etat_api=etat_api(), data=data)
+	return render_template("supervision.html", etat_api=etat_api())
+
+@app.route("/superv-data")
+def superv_data():
+	raw_res = requests.get(f"{api_url}ED&CO_DATE={periode}")
+	res = raw_res.json()
+	data = res['Data']['Data']
+	postes_set = set()
+	for el in data:
+		postes_set.add(el['PO_NOM'])
+	postes = [{"po_nom": value, "po_usernom": "",
+			   "appels": 0, "duree": 0, "cout": 0}
+			   for value in postes_set]
+	for el in data:
+		poste = [e for e in postes if e['po_nom'] == el['PO_NOM']][0]
+		poste['po_usernom'] = el['PO_USERNOM']
+		poste['appels'] += 1
+		poste['duree'] += el['CO_DUR']
+		poste['cout'] += el['CO_COUTFACT_TTC']
+	for item in postes:
+		item['duree'] = str(datetime.timedelta(seconds = item['duree']))
+
+	return jsonify(postes)
 
 @app.route("/facturation")
 def facturation():
