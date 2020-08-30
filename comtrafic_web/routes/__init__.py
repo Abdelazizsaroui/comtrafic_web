ac78efedeff4353ba93fce4a5488c667e2a5ef39
abdelaziz saroui <saroui2015gmail.com>
2020-08-30 14:51:51 +0100

    add api_port as variable and change port to 7072

--- comtrafic_web/routes/__init__.py
+++ comtrafic_web/routes/__init__.py
@@ -1,11 +1,12 @@
 import requests
 
-api_url = "http://161.97.75.12:7071/api/cmd/"
+api_port = 7072
+api_url = f"http://161.97.75.12:{api_port}/api/cmd/"
 periode = "43666-43673"
 
 def etat_api():
 	try:
-		requests.get("http://161.97.75.12:7071/api/", timeout=1)
+		requests.get(f"http://161.97.75.12:{api_port}/api/", timeout=1)
 		return 1
 	except:
 		return 0
